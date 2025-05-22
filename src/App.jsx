939075b53c1ae0fe17643d313a2f0e71162a948b
sowmyareddy705 <23r11a6705@gcet.edu.in>
2025-05-22 15:35:59 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Product from "./components/Product";
+import Cart from "./components/Cart";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 function App() {
-  
   return (
-    <>
-      <header>
-        <h1>My shop</h1>
-        <hr/>
-      </header>
-      <main>
-<h2> product list</h2>
-      </main>
-      <footer>
-        <hr/>
-        &copy 2025.all rights Reserved.
-      </footer>
-    </>
-  )
-}
+    <div>
+      <BrowserRouter>
+        <header>
+          <h1>My Online Shop</h1>
+          <Link to="/">Home</Link>-
+          <Link to="/cart">Cart</Link>
+          <hr />
+        </header>
 
-export default App
+        <main>
+          <Routes>
+            <Route index element={<Product />} />
+            <Route path="/" element={<Product/>}></Route>
+            <Route path="/cart" element={<Cart/>}></Route>
+          </Routes>
+        </main>
+
+        <footer>
+          <hr />
+          &copy; 2005. All rights Reserved.
+        </footer>
+      </BrowserRouter>
+    </div>
+  );
+}
+export default App;
